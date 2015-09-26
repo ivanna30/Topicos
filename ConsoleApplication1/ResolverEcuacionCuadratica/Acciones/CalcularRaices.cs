9ae8793826512235d2157196dfa6f905a5da2e35
ivanna30 <ivanna.chacon@hotmail.es>
2015-09-26 15:06:45 -0600

    Se agregan las soluciones faltantes

---
+++ ConsoleApplication1/ResolverEcuacionCuadratica/Acciones/CalcularRaices.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResolverEcuacionCuadratica.Acciones
+{
+    class CalcularRaices
+    {
+
+        public void CalcularLasRaices(double discriminante, double primerNumero, double segundoNumero, ref double primeraRaiz, ref double segundaRaiz)
+        {
+
+            primeraRaiz = (-segundoNumero + Math.Sqrt(discriminante)) / (2 * primerNumero);
+            segundaRaiz = (-segundoNumero + Math.Sqrt(discriminante)) / (2 * primerNumero);
+        }
+    }
+}
---
+++ ConsoleApplication1/ResolverEcuacionCuadratica/Especificaciones/ResolverEcuacionCuadratica.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResolverEcuacionCuadratica
+{
+    class ResolverEcuacionCuadratica
+    {
+        public void ResolverLaEcuacionCuadratica(double primerNumero, double segundoNumero, double tercerNumero, ref double primeraRaiz, ref double segundaRaiz)
+        {
+
+            //calcular el discriminante
+            CalcularDiscriminante accionCalcularDiscriminante = new CalcularDiscriminante();//clase de la instancia de ese metodo
+            double discriminante = accionCalcularDiscriminante.CalcularElDiscriminante(primerNumero, segundoNumero, tercerNumero);
+
+            //validar el discriminante
+            ValidarDiscriminante validardiscriminante = new ValidarDiscriminante();
+            if (validardiscriminante.ValidarDiscriminanteMayorQuecero(discriminante))
+            {
+
+                //Calcular raices
+                CalcularRaices calcularRaices = new CalcularRaices();
+                calcularRaices.CalcularLasRaices(discriminante, primerNumero, segundoNumero, tercerNumero, ref primeraRaiz, ref segundaRaiz);
+            }
+            else
+            {
+                //levante una excepción
+                Exception ex = new Exception(String.Format("ocurrió un error al calcular las raices," + "ya que [{1}] es mayor que el discriminante [{0}] es menor que cero.", discriminante.ToString()), "0.0");
+                throw ex;//aborta este metodo y le pasa la excepcion a quien haya embocado la especificacion
+            }
+        }
+    }
+}
